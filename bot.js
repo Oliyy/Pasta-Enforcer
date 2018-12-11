29c31f8dc03a59b174a4544b1fd460a42713f538
Oliyy <under5zzs@gmail.com>
2018-12-11 23:49:11 +0100

    First commit

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const pasta = ["Spaghetti", "Macaroni", "Ravioli", "Cannelloni", "Linguine", "Lasagne", "Rigatoni", "Tortellini", "Penne", "Fettuccine", "Pappardelle", "Orzi", "Bucatini", "Fusilli", "Vermicelli", "Orecchiette", "Fregula", "Taglierini", "Farfalle", "Manicotti", "Cavatelli", "Paccheri", "Capellini", "Agnolotti", "Strozzapreti", "Cavatappi", "Ziti", "Trofie", "Garganelli", "Conchiglie", "Casarecce", "Rotini", "Pizzoccheri", "Ditalini", "Ptitim", "Gemelli", "Scialatielli", "Cappelletti", "Capelli d’angelo", "Bigoli", "Calamarata", "Mezzelune", "Bavette", "MeMes"]
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.author.bot == true) {
+    } else {
+  let randPasta = pasta[Math.floor(Math.random()*pasta.length)];
+  console.log(randPasta)
+    console.log(pasta.indexOf(msg.member.displayName) == -1)
+    // if (pasta.indexOf(msg.member.displayName) == -1) {
+      msg.member.setNickname(randPasta)
+        .then(console.log("Changed " + msg.author.username + "'s succesfully"))
+        .catch(console.error);
+    // }
+}
+});
+
+client.login('token');
